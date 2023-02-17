1674004216a8916a0fc7282c5b4ceb53fd7c1376
mariam175 <mariamrafaat28@gmail.com>
2023-02-17 05:48:59 -0800

    initial commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+			continue;
+		}
+		else
+		{
+			putchar(a);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
