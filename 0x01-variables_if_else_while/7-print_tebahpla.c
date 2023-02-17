c44c922c6e024282d078c4ce73e0f87e941ac3c5
mariam175 <mariamrafaat28@gmail.com>
2023-02-17 06:07:08 -0800

    initial commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'z'; a >= 'a'; a--)
+	{
+		putchar(a);
+	}
+	putchar(a);
+	return (0);
+}
