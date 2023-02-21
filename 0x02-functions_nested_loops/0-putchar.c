ce54012a4277ff0b36865decc80361bae6a27c12
mariam175 <mariamrafaat28@gmail.com>
2023-02-21 08:24:56 -0800

    initial commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <unistd.h>
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -7,6 +8,13 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char str[8] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 	return (0);
 }
