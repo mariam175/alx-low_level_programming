5af76c66bff76c884be0df72726118f14f00271f
mariam175 <mariamrafaat28@gmail.com>
2023-02-21 08:30:42 -0800

    initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; i < 'z'; i++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return (0);
+}
