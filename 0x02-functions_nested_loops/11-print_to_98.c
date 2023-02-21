69cd7824cfdf0e7e64c206900be9268c5c310bbf
mariam175 <mariamrafaat28@gmail.com>
2023-02-21 10:55:37 -0800

    initial commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * print_to_98 - Entry point
+ * @n: integer number
+ * Return: numbers between n and 98
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n < 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			_putchar(i + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			_putchar(i + '0');
+			if (i != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
