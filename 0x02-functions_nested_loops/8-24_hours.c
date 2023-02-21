d3d7d7cc1960368de227c5d6d8e5605fee2c05f5
mariam175 <mariamrafaat28@gmail.com>
2023-02-21 10:41:03 -0800

    initial commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (int h = 0; h < 24; h++)
+	{
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+		}
+	}
+}
