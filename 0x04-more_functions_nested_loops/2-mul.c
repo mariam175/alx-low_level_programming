2f980319bc9d06d9a2bde3d5befa0de52fd3194e
mariam175 <mariamrafaat28@gmail.com>
2023-02-23 09:09:05 -0800

    initial commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - Entry point
+ *@a: integer
+ *@b: integer
+ * Return: mul of two numbers
+ */
+int mul(int a, int b)
+{
+	int m;
+
+	m = a * b;
+	return (m);
+}
