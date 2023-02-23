df9e4c8931e3e01d4ec985f9e55487fef5153966
mariam175 <mariamrafaat28@gmail.com>
2023-02-23 10:23:10 -0800

    initial commit

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char fizz[] = "Fizz";
+	char buzz[] = "Buzz";
+	char fizzbuzz[] = "FizzBuzz";
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+		{
+			printf("%s", fizz);
+		}
+		else if (i % 5 == 0)
+		{
+			printf("%s", buzz);
+		}
+		else if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("%s", fizzbuzz);
+		}
+		else
+		{
+			printf("%d", i);
+		}
+	}
+	prinf('\n');
+	return (0);
+}
