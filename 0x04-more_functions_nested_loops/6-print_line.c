2f5e6481d9410f8e1af1af36395a488f29bc110c
mariam175 <mariamrafaat28@gmail.com>
2023-02-23 09:42:06 -0800

    initial commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - Entry point
+ *@n: integer
+ * Return: Always 0 (Success)
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
