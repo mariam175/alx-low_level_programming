eb654db99410dd895ef91cd6c92880266e2ffdf1
mariam175 <mariamrafaat28@gmail.com>
2023-02-23 10:06:57 -0800

    initial commit

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - Entry point
+ *@size: integer
+ * Return: Always 0 (Success)
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
