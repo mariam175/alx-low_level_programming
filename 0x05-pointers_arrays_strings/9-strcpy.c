4303b8ef381cc8d1f6942d14d6f6e43de9ae76cf
mariam175 <mariamrafaat28@gmail.com>
2023-02-27 12:49:35 -0800

    initial commit

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strcpy - Entry point
+ * @dest: string
+ * @src : string
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
