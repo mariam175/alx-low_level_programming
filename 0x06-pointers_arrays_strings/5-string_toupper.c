bb1a9fd77b7f88bbecae2abab4e5e0eed7c204a6
mariam175 <mariamrafaat28@gmail.com>
2023-03-02 10:04:24 -0800

    initial commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * string_toupper - Entry point
+ * @s: string
+ * Return: uppercase of string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		s[i] -= 32;
+	}
+	return (s);
+}
