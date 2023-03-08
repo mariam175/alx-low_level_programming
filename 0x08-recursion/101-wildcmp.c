01851862ac2798522d9e35328bdf50a862b094f8
mariam175 <mariamrafaat28@gmail.com>
2023-03-08 15:14:22 -0800

    initial commit

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * wildcmp - Entry point
+ *@s1: string
+ *@s2: string
+ * Return: 1 if the strings can be considered identical, otherwise return 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s1 != *s2 && *s2 != '*')
+		return (0);
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	return (1);
+}
