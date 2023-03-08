e3b52c6b5eb63eb3ba4fc5925938c0384d8e42f2
mariam175 <mariamrafaat28@gmail.com>
2023-03-08 14:36:15 -0800

    initial commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -33,3 +33,14 @@ int palindrome(char *s, int l, int r)
 		return (palindrome(s, l + 1, r - 1));
 	return (1);
 }
+/**
+ * _strlen_recursion - Entry point
+ *@s:string
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
+}
