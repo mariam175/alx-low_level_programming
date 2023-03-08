76130d3eeae30cc286884e36a389ab3561dec00c
mariam175 <mariamrafaat28@gmail.com>
2023-03-08 11:15:33 -0800

    initial commit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+int is_prime(int n, int i);
+/**
+ * is_prime_number- Entry point
+ *@n: integer
+ * Return: is prime or not
+ */
+int is_prime_number(int n)
+{
+	is_prime(n, n - 1);
+}
+/**
+ * is_prime- Entry point
+ *@n: integer
+ *@i: integer
+ * Return: is prime or not
+ */
+int is_prime(int n, int i)
+{
+	if (i < 2)
+		return (0);
+	else if (i == 2 || i == 3)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	return (is_prime(n, i - 1));
+}
