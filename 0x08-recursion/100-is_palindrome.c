52bcca9d2a03c3121560fc7180d8256166b4d500
mariam175 <mariamrafaat28@gmail.com>
2023-03-08 14:25:27 -0800

    initial commit

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+int palindrome(char *s, int l, int r);
+/**
+ * is_palindrome - Entry point
+ *@s: string
+ * Return: is palindrom or not
+ */
+int is_palindrome(char *s)
+{
+	int i, len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		len++;
+	if (len == 0)
+		return (1);
+	return (palindrome(s, 0, len - 1));
+}
+
+/**
+ *palindrome - Entry point
+ *@s:string
+ *@l: integer
+ *@r: integer
+ * Return: is palindrom or not
+ */
+int palindrome(char *s, int l, int r)
+{
+	if (l == r)
+		return (1);
+	if (s[l] != s[r])
+		return (0);
+	if (l < r)
+		return (palindrome(s, l + 1, r - 1));
+}
