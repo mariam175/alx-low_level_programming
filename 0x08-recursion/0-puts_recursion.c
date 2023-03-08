07b423403523ff2831466cf85bee465aef8535c9
mariam175 <mariamrafaat28@gmail.com>
2023-03-08 07:48:19 -0800

    initial commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts_recursion - Entry point
+ *@s:string
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	*s++;
+	_puts_recursion(s);
+}
