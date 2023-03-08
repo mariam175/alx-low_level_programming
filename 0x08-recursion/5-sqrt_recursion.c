d47fd6b33b876d33077384b05aefac0508f4c6f3
mariam175 <mariamrafaat28@gmail.com>
2023-03-08 09:37:00 -0800

    initial commit

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+int sqrtt(int n, int x);
+/**
+ * _sqrt_recursion - Entry point
+ *@n: integer
+ * Return: sqrt of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (sqrtt(n, (n + 1) / 2));
+}
+/**
+ * sqrtt - Entry point
+ *@n: integer
+ *@x: integer
+ * Return: sqrt
+ */
+int sqrtt(int n, int x)
+{
+	if (x < 1)
+		return (-1);
+	if (x * x == n)
+		return (x);
+	return (sqrtt(n, x - 1));
+}
