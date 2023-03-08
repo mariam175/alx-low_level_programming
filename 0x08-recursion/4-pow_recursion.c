939f4ee13e7288d66b8a30620852d2b0513c1f90
mariam175 <mariamrafaat28@gmail.com>
2023-03-08 08:33:24 -0800

    initial commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _pow_recursion - Entry point
+ *@x: integer
+ *@y: integer
+ * Return: the value of x raised to the power of y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	return (_pow_recursion(x, y - 1) * x);
+}
