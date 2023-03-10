968e25df246e15571ae02d0999ccaec2db8995da
mariam175 <mariamrafaat28@gmail.com>
2023-03-10 06:15:40 -0800

    argc_argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,11 +6,7 @@
  *@argv:array point to arguments
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv)
 {
-	int i = 0;
-
-	for (; i < argc; i++)
-		;
-	return (i - 1);
+	return (argc - 1);
 }
