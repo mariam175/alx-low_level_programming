f0b92f89b1052290505c1444c98defc9da8e11e3
mariam175 <mariamrafaat28@gmail.com>
2023-03-10 05:45:56 -0800

    argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *@argc:number of arguments
+ *@argv:array point to arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
