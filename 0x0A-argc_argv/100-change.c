26a967ad44a6e357dbbe6a3dc0ed6c12f1b6d1e1
mariam175 <mariamrafaat28@gmail.com>
2023-03-10 17:13:30 -0800

    argc-argv

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,32 +12,38 @@ int main(int argc, char *argv[])
 {
 	int coin = 0, m;
 
-	if (argc != 2)
+	if (argc == 2)
 	{
-		printf("Error\n");
-		return (1);
-	}
-	if (strchr(argv[argc - 1], '-'))
-	{
-		printf("0\n");
-		return (1);
-	}
-	m = atoi(argv[argc - 1]);
+		if (strchr(argv[argc - 1], '-'))
+		{
+			printf("0\n");
+			return (1);
+		}
+		m = atoi(argv[argc - 1]);
 
-	while (m > 0)
-	{
-		if (m % 25 == 0)
-			m -= 25;
-		else if (m % 10 == 0)
-			m -= 10;
-		else if (m % 5 == 0)
-			m -= 5;
-		else if (m % 2 == 0)
-			m -= 2;
-		else
-			m--;
-		coin++;
+		while (m > 0)
+		{
+			if (m % 25 == 0)
+			{
+				m -= 25;
+			} else if (m % 10 == 0)
+			{
+				m -= 10;
+			} else if (m % 5 == 0)
+			{
+				m -= 5;
+			} else if (m % 2 == 0)
+			{
+				m -= 2;
+			} else
+			{
+				m--;
+			}
+			coin++;
+		}
+		printf("%d\n", coin);
+		return (0);
 	}
-	printf("%d\n", coin);
-	return (0);
+	printf("Error\n");
+	return (1);
 }
