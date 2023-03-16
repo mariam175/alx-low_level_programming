3a24fd083d38c2b8df0d7739172322d21d2ab869
mariam175 <mariamrafaat28@gmail.com>
2023-03-16 12:57:20 -0700

    more_malloc_free

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -7,8 +7,6 @@
  *@size:int
  * Return: Nothing
  */
-
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	int *arr;
@@ -19,7 +17,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	arr = malloc(nmemb * size);
 	if (arr == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 		arr[i] = 0;
 	return (arr);
 }
