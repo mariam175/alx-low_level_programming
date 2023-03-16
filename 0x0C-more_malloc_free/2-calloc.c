d1a4071c361ac77c3b2dabf22a5066409adeb61a
mariam175 <mariamrafaat28@gmail.com>
2023-03-16 12:53:13 -0700

    more_malloc_free

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_calloc - Entry point
+ *@nmemb:int
+ *@size:int
+ * Return: Nothing
+ */
+
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+	if (arr == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		arr[i] = 0;
+}
