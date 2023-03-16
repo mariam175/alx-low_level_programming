10cccfcb7847808786faff550774a4b905edc343
mariam175 <mariamrafaat28@gmail.com>
2023-03-16 14:52:52 -0700

    more_malloc_free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_realloc - Entry point
+ *@ptr:is a pointer to the memory previously allocated
+ *@old_size:is the size, in bytes, of the allocated space for ptr
+ *@new_size:is the new size, in bytes of the new memory block
+ * Return: ptr
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	if (new_size == old_size)
+		return (ptr);
+	if (ptr == NULL)
+		ptr = malloc(new_size);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	free(ptr);
+	ptr = malloc(new_size);
+	return (ptr);
+}
