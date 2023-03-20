1a39fe51a93355bf76f077e3bf54ec8acf5deb19
mariam175 <mariamrafaat28@gmail.com>
2023-03-20 13:53:45 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,7 +18,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	nedog = malloc(sizeof(dog_t));
 	if (name == NULL)
 		return (NULL);
-	if (name == NULL || age <= 0 || owner == NULL)
+	if (nedog == NULL || age <= 0 || owner == NULL)
 	{
 		free(nedog);
 		return (NULL);
