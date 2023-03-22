29517ff18f98c264a37187213800a74a51f3f1df
mariam175 <mariamrafaat28@gmail.com>
2023-03-22 10:45:27 -0700

    function-pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ *print_name - prints a name
+ *@name:the name
+ *@f:pointer to func
+ *Return:nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef __pointer_to_func__
+#define __pointer_to_func__
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+#endif
