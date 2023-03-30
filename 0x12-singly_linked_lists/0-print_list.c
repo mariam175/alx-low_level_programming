cb55864b01fd15a09afd0c1bc28996cb38ecbd33
mariam175 <mariamrafaat28@gmail.com>
2023-03-30 07:22:34 -0700

    single linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *print_list - ...
+ *@h:linked list
+ *Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t cnt = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil\n)");
+		else
+			printf("[%d] (%s)\n", h->len, h->str);
+		h = h->next;
+		cnt++;
+	}
+	return (cnt);
+}
