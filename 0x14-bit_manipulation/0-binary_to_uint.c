5aa4760aacb7b5190f575ad813f97b7b82ec0f77
mariam175 <mariamrafaat28@gmail.com>
2023-04-08 10:39:11 -0700

    bits

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -22,7 +22,8 @@ unsigned int binary_to_uint(const char *b)
 		if (b[i] != '0' || b[i] != '1')
 			return (0);
 
-		res += (b[i] - '0') * (1 << pow);
+		if (b[i] == '1')
+			res += 1 << pow;
 		pow++;
 	}
 	return (res);
