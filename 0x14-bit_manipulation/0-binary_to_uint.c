9156ecaffc4abdfa1c843849f27f061790780974
mariam175 <mariamrafaat28@gmail.com>
2023-04-08 09:13:08 -0700

    bits

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ *binary_to_uint- ...
+ *@b: ....
+ *Return:the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int res = 0;
+	int len = 0, pow = 0, i;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+	for (len = 0; b[len] != '\0'; len++)
+		;
+	for (i = len - 1; i >= 0; i--)
+	{
+		if (b[i] != '0' || b[i] != '1')
+			return (0);
+
+		res += (b[i] - '0') * (1 << pow);
+		pow++;
+	}
+	return (res);
+}
