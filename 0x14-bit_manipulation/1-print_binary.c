0ffc6882218c4ccfa42a20c34865d46fcae91dc1
mariam175 <mariamrafaat28@gmail.com>
2023-04-08 12:13:17 -0700

    bits

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ *binary- ...
+ *@n: ....
+ *Return: nothing
+ */
+void binary(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
+
+/**
  *print_binary- ...
  *@n: ....
  *Return: nothing
@@ -9,7 +22,7 @@
 void print_binary(unsigned long int n)
 {
 	if (n == 0)
-		return;
-	print_binary(n >> 1);
-	_putchar((n & 1) + '0');
+		_putchar('0');
+	else
+		binary(n);
 }
