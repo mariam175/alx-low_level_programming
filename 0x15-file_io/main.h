2191d8841284d429575baa552ba9b444f466aebb
mariam175 <mariamrafaat28@gmail.com>
2023-04-10 16:04:41 -0700

    file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,13 @@
-#ifndef __FILE__
-#define __FILE__
-
+#ifndef FILE_H_
+#define FILE_H_
+#include <stdio.h>
+#include <stdlib.h>
 #include <sys/types.h>
-#include <stddef.h>
-char _putchar(char c);
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-
 #endif
