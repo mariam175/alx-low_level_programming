e4343674c47bb53f94284914ba4e8621080e7d2c
mariam175 <mariamrafaat28@gmail.com>
2023-04-10 11:20:48 -0700

    bits

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ *get_bit- ...
+ *@n: ....
+ *@index:...
+ *Return:the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int indx;
+
+	if (index < 63)
+		return (-1);
+	return ((n >> index) & 1);
+}
