b72fd8a557c403ac36d985bd8d10613c9d9bee65
mariam175 <mariamrafaat28@gmail.com>
2023-04-10 17:12:04 -0700

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,10 +18,12 @@ int create_file(const char *filename, char *text_content)
 	f = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (f == -1)
 		return (-1);
-	for (len = 0; text_content[len] != '\0'; len++)
-		;
 	if (text_content)
+	{
+		for (len = 0; text_content[len] != '\0'; len++)
+			;
 		write(f, text_content, len);
+	}
 	close(f);
 	return (1);
 }
